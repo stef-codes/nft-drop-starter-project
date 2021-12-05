4ac2a8a87d0ccf15ada4463aacbdae3772a4a0b8
Stefanie <stefcodes@gmail.com>
2021-12-04 16:22:42 -0800

    add logic for phantom wallet

--- app/src/App.js
+++ app/src/App.js
@@ -7,12 +7,47 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // Actions 
+
+  /* Declare your function */
+
+  const CheckIfWalletIsConnected = async () => {
+
+    try{ 
+      const {solana} = window; 
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom Wallet Found!'); 
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻'); 
+      }
+    } catch (error) {
+      console.error(error); 
+    }
+  }; 
+
+    /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+
+    useEffect(() => {
+      const onLoad = async() => {
+      await CheckIfWalletIsConnected(); 
+    }; 
+    window.addEventListener('load', onload); 
+    return () => window.removeEventListener('load', onLoad); 
+  }, []); 
+
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
+          <p className="header">🍭 Music Drop</p>
+          <p className="sub-text">Mint Loops as NFTs </p>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
